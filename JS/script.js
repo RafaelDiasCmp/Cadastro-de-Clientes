9142a072ddde91cd23c34b4f3fda80394b168dcd
Rafael Dias Campos <raffaeldiascampos@gmail.com>
2024-05-03 13:23:39 -0300

    closes #3 API CEP

--- JS/script.js
+++ JS/script.js
@@ -17,12 +17,19 @@ function search() {
 
 
 function save() {
+    
     var infoCliente = {
 
     id: cliente.length + 1,
     primaryName: document.getElementById("primaryName").value,
     secondaryName: document.getElementById("secondaryName").value,
     cep: document.getElementById("cep").value,
+    address: document.getElementById("address").value,
+    number: document.getElementById("numberHouse").value,
+    neighborhood: document.getElementById("neighborhood").value,
+    city: document.getElementById("city").value,
+    state: document.getElementById("state").value
+
 };
     
     cliente.push(infoCliente);
