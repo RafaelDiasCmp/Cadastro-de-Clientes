b9101a096eaf20b4cfdbae0a60f329acaa801895
Rafael Dias Campos <raffaeldiascampos@gmail.com>
2024-05-03 12:12:12 -0300

    closes #2 Botão salvar funcional

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+var cliente = [];
+
+function save() {
+    var infoCliente = {
+
+    id: cliente.length + 1,
+    primaryName: document.getElementById("primaryName").value,
+    secondaryName: document.getElementById("secondaryName").value,
+    cep: document.getElementById("cep").value,
+};
+    
+    cliente.push(infoCliente);
+    console.log(infoCliente);
+}
